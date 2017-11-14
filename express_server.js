0e529e437e9521a3f127142770d7328ac02bf7d4
Joseph Cortina <jlpcort6@gmail.com>
2017-11-14 02:57:46 +0000

    Fixed urls page crashing-changed scriplet in heading

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Tiny URL app:
 Author:   Joseph Cortina
 Date:     October 11, 2017
@@ -9,27 +9,47 @@ Description:
 */
 const alphaNum = ["a", "b", "0", "c", "d", "1", "e", "f", "2", "g", "h", "3", "i", "j", "4", "k", "l", "5", "m", "n", "6", "o", "p", "7", "q", "r", "8", "s", "t", "9", "u", "v", "w", "x", "y", "z"];
 
-var users = { 
+var users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "bob@example.com", 
+    id: "userRandomID",
+    email: "bob@example.com",
     password: "bobisyouruncle"
   },
  "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   },
   "jh8H6m": {
-    id: "jh8H6m", 
-    email: "tim@example.com", 
+    id: "jh8H6m",
+    email: "tim@example.com",
     password: "timthetoolman"
   }
 }
 var urlDatabase = {
-  "b2xVn2": 
+  "b2xVn2":
     { userID: "userRandomID",
          url: "http://www.lighthouselabs.ca"
+    },
+  "b2xVn3":
+    { userID: "userRandomID",
+         url: "http://www.thegreatpub.ca"
+    },
+  "b2xVn4":
+    { userID: "userRandomID",
+         url: "http://www.jusstanotherlongname.com"
+    },
+  "b2xVn5":
+    { userID: "userRandomID",
+         url: "http://www.anotheruselesswebsite.com"
+    },
+  "b2xVn6":
+    { userID: "userRandomID",
+         url: "http://www.yetanotherwebsite.ca"
+    },
+  "b2xVn7":
+    { userID: "userRandomID",
+         url: "http://www.prettydamgoodone.com"
     },
   "9sm5xK":
     { userID: "user2RandomID",
@@ -54,13 +74,18 @@ function genRandString()  {
     tinyString = tinyString + charSel;
   }
   return tinyString;
-} 
+}
 // filter urlsDatabase; return only urls for a given user
-function urlsForUser(id)  {
+function urlsForUser(id, loggedIn)  {
   let urls = [];
   for (let tinyURL in urlDatabase)  {
-    if (urlDatabase[tinyURL].userID === id)  {
-      urls.push({"tinyURL": tinyURL, "bigURL": urlDatabase[tinyURL].url});
+    //if id is not null include only for specific user, else all
+    if (loggedIn) {
+      if (urlDatabase[tinyURL].userID === id)  {
+        urls.push({"tinyURL": tinyURL, "bigURL": urlDatabase[tinyURL].url});
+      }
+      } else {
+        urls.push({"tinyURL": tinyURL, "bigURL": urlDatabase[tinyURL].url});
     }
   }
   return urls;
@@ -73,7 +98,7 @@ var app = express();
 app.use(cookieParser());
 
 var PORT = process.env.PORT || 8080; // default port 8080
-const domain = "http://localhost:8080/"
+const domain = "http://localhost:8080/";
 
 app.set("view engine", "ejs");
 
@@ -87,14 +112,20 @@ app.get("/", (req, res) => {
   if ("user_id" in req.cookies) {
     res.redirect(domain + "urls");
   } else {
-    res.redirect(domain + "login"); 
-  } 
+    res.redirect(domain + "login");
+  }
 });
 
 app.get("/urls", (req, res) => {
-  const user_id = req.cookies.user_id;
-  const templateVars = {urls: urlDatabase, root: domain, user: users[user_id]};
-  res.render("urls_index", {templateVars});
+  //const user_id = req.cookies.user_id;
+  const user_id = "userRandomID";
+  //const user_id = null;
+  const loggedIn = "user_id" in req.cookies ? true : false;
+  //get subset of urls for this user
+  let userUrls = urlsForUser(user_id, loggedIn);  
+  const templateVars = {root: domain, userID: user_id, urls: userUrls, loggedIn: loggedIn, user: users[user_id]};
+  console.log(templateVars);
+  res.render("urls_index", templateVars); 
 });
 //create a new tiny url -only allow for registered users
 app.get("/urls/new", (req, res) => {
@@ -108,7 +139,7 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect(domain + "urls");
 });
-// process the Get login 
+// process the Get login
 app.get("/login", (req, res) => {
   res.render("login");
 });
@@ -118,7 +149,7 @@ app.post("/login", (req, res) => {
   if (req.body.email === "" || req.body.password === "")  {
     res.status(400);
     res.send("ERROR: email or password missing");
-  }   
+  }
   //ensure email exists, otherwise error
   let emailFound = false;
   for (var user in users) {
@@ -126,7 +157,7 @@ app.post("/login", (req, res) => {
       emailFound = true;
       break;
     }
-  }  
+  }
   //if email not found, send error page
   if (!emailFound)  {
     res.status(403);
@@ -139,13 +170,13 @@ app.post("/login", (req, res) => {
   }
   console.log(user);
   res.cookie("user_id", user);
-  res.redirect(domain);  
+  res.redirect(domain);
 });
 // log-out requested - clear cookie, remove from templateVars
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect(domain);
-  
+
 });
 // get method for update URL page
 app.get("/urls/:id/update", (req, res) => {
@@ -154,13 +185,14 @@ app.get("/urls/:id/update", (req, res) => {
 // update a long URL for a given short URL
 app.post("/urls/:id/update", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect(domain + "urls"); 
+  res.redirect(domain + "urls");
 });
 // get request for the registration page
 app.get("/register", (req, res) => {
   if ("user_id" in req.cookies) {
     res.redirect(domain + "urls");
   } else {
+    console.log("im here");
   res.render("user_register");
   }
 });
@@ -179,7 +211,7 @@ app.post("/register", (req, res) => {
       res.send("ERROR: email already exists");
     return;
     }
-  }  
+  }
   //gen user id, add it and email, password to users database
   let newID = genRandString();
   console.log(newID);
@@ -190,13 +222,12 @@ app.post("/register", (req, res) => {
   let passHash = bcrypt.hashSync(passwrd, 10);
   users[newID].password = passHash;
   res.cookie("user_id", newID);
-  console.log(newID, users[newID].email, passwrd, users[newID].password);  
+  console.log(newID, users[newID].email, passwrd, users[newID].password);
   res.redirect(domain + "urls");
 });
 app.post("/urls", (req, res) => {
-  // get tiny URL, add it and long one to urlDatabase
   let tinyString = genRandString();
-  urlDatabase[tinyString] = req.body.longURL; 
+  urlDatabase[tinyString] = req.body.longURL;
   // redirect response to urlRoot + tinyString
   res.redirect(domain + 'urls' + tinyString);
 });
@@ -215,10 +246,6 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Example app listening on port ${PORT}!`);
-}) 
+})
